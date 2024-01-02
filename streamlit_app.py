6de317aa8d8ca75003c3446053aaf35665c64635
123033-osaka <osaka_masaki@comture.com>
2024-01-02 17:34:20 +0900

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('st.secrets')
 
 st.write(st.secrets['message'])
+
+st.write('sample_usernameの値は: ' + st.secrets['sample_username'])
+st.write('sample_passwordの値は: ' + st.secrets['sample_password'])
