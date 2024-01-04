67c73232f8cbb09cf5d50c1c31034f8bf4633ccf
123033-osaka <osaka_masaki@comture.com>
2024-01-04 12:07:35 +0900

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,38 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from time import time
 
-# タイトルを表示
-st.title('st.experimental_get_query_params')
+st.title('st.cache')
 
-# URLパラメータを取得して表示
-params = st.experimental_get_query_params()
-params = http://localhost:8501/?show_map=True&selected=asia&selected=america
-if params is None:
-    st.write('get_query_params(): None')
-else:
-    st.write('get_query_params(): not None')
-if isinstance(params, dict):    #paamsが辞書旗ならば発動する
-    st.write('get_query_params(): dict')
-    l = len(params)
-    st.write('get_query_params(): len == {}'.format(l))
-else:
-    st.write('get_query_params(): not dict')
-st.write(params)
+# Using cache
+a0 = time()
+st.subheader('Using st.cache')
+
+@st.cache(suppress_st_warning=True)
+def load_data_a():
+    df = pd.DataFrame(
+    np.random.rand(2000000, 5),
+    columns=['a', 'b', 'c', 'd', 'e']
+    )
+    return df
+
+st.write(load_data_a())
+a1 = time()
+st.info(a1-a0)
+
+
+# Not using cache
+b0 = time()
+st.subheader('Not using st.cache')
+
+def load_data_b():
+    df = pd.DataFrame(
+    np.random.rand(2000000, 5),
+    columns=['a', 'b', 'c', 'd', 'e']
+    )
+    return df
+
+st.write(load_data_b())
+b1 = time()
+st.info(b1-b0)
