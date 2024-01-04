fdf25852b8ad6fcdb760e027f2353db9be472167
123033-osaka <osaka_masaki@comture.com>
2024-01-04 11:02:42 +0900

    initail

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,18 @@
 import streamlit as st
 
+# タイトルを表示
 st.title('st.experimental_get_query_params')
 
-with st.expander('About this app'):
-  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
-
-# 1. Instructions
-st.header('1. Instructions')
-st.markdown('''
-In the above URL bar of your internet browser, append the following:
-`?firstname=Jack&surname=Beanstalk`
-after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
-such that it becomes 
-`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
-''')
-
-
-# 2. Contents of st.experimental_get_query_params
-st.header('2. Contents of st.experimental_get_query_params')
-st.write(st.experimental_get_query_params())
-
-
-# 3. Retrieving and displaying information from the URL
-st.header('3. Retrieving and displaying information from the URL')
-
-firstname = st.experimental_get_query_params()['firstname'][0]
-surname = st.experimental_get_query_params()['surname'][0]
-
-st.write(f'Hello **{firstname} {surname}**, how are you?')
+# URLパラメータを取得して表示
+params = st.experimental_get_query_params()
+if params is None:
+    st.write('get_query_params(): None')
+else:
+    st.write('get_query_params(): not None')
+if isinstance(params, dict):
+    st.write('get_query_params(): dict')
+    l = len(params)
+    st.write('get_query_params(): len == {}'.format(l))
+else:
+    st.write('get_query_params(): not dict')
+st.write(params)
